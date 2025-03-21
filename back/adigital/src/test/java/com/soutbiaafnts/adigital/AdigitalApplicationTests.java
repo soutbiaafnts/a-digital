ea3a8c5c42d74eaf5def83e21753db9fd98b2832
soutbiaafnts <bia.fontes653@gmail.com>
2025-03-21 15:01:19 -0300

    feat: initial commit

---
+++ back/adigital/src/test/java/com/soutbiaafnts/adigital/AdigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soutbiaafnts.adigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
